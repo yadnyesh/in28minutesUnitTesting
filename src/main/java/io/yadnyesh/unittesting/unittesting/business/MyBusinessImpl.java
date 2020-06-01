09d7a6b732f35d5431b989374486c4039bd22c6f
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-06-01 19:16:31 +0530

    Wrote some more sample tests

--- src/main/java/io/yadnyesh/unittesting/unittesting/business/MyBusinessImpl.java
+++ src/main/java/io/yadnyesh/unittesting/unittesting/business/MyBusinessImpl.java
@@ -1,6 +1,12 @@
 package io.yadnyesh.unittesting.unittesting.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.yadnyesh.unittesting.unittesting.data.SomeDataService;
+
 public class MyBusinessImpl {
+	
+	SomeDataService someDataService;
 	
 	public int calculate(int[] data) {
 		int sum = 0;
@@ -9,5 +15,23 @@ public class MyBusinessImpl {
 		}
 		return sum;
 	}
+	
+	public int calculateSumUsingDataService() {
+		int sum = 0;
+		int[] data = someDataService.retrieveAllData();
+		for(int value:data) {
+			sum += value;
+		}
+		return sum;
+	}
+
+	public SomeDataService getSomeDataService() {
+		return someDataService;
+	}
+
+	public void setSomeDataService(SomeDataService someDataService) {
+		this.someDataService = someDataService;
+	}
+	
 
 }
